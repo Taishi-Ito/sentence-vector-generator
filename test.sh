4928f0e60916530e32bd813f5c6b1fae4d33ad1f
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-24 09:39:48 +0900

    feat:Create shell script to run test

---
+++ test.sh
@@ -0,0 +1,1 @@
+docker run --rm -v ~/sentence-vector-generator:/sentence-vector-generator -w /sentence-vector-generator -p 8000:8000 --name sentence-vector-generator -it sentence-vector-generator pytest
